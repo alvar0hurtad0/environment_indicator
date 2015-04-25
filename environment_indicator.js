1f1df7fd3782de51ad611b01dc11293411e6f2cb
Alvaro Hurtado <alvaro@dsmultimedia.es>
2015-04-25 17:46:39 +0200

    Initial commit.

---
+++ environment_indicator.js
@@ -0,0 +1,3 @@
+var d = document.createElement("div");
+d.setAttribute("style", "background-color: red; height: 100%;width: 3px; position: fixed;bottom:0; top:0; left:0; z-index: 9999;");
+document.body.appendChild(d);
